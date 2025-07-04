40057f993386ed95796c9d409c298a9c88ed3576
umut <alizzaade@users.noreply.github.com>
2025-07-04 17:57:51 +0200

    Add Update and Delete methods

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -7,13 +7,9 @@ namespace eCommerce.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController(StoreContext context) : ControllerBase
     {
-        private readonly StoreContext context;
-        public ProductsController(StoreContext context)
-        {
-            this.context = context;
-        }
+        private readonly StoreContext context = context;
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
@@ -22,19 +18,43 @@ namespace eCommerce.Controllers
         }
 
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<Product>> GetProducts(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
             var product = await context.Products.FindAsync(id);
             if (product == null) return NotFound();
             return product;
         }
 
-        [HttpGet]
+        [HttpPost]
         public async Task<ActionResult<Product>> CreateProduct(Product product)
         {
             context.Products.Add(product);
             await context.SaveChangesAsync();
             return product;
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateProduct(Product product, int id)
+        {
+            if (product.Id != id || !ProductExists(id)) return BadRequest("Cannot update this product!");
+            context.Entry(product).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return context.Products.Any(x => x.Id == id);
         }
     }
 }
