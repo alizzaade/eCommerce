8c8c0d382662073ff2b2eb99c8bdb1453f525b0c
umut <alizzaade@users.noreply.github.com>
2025-07-23 13:52:42 +0200

    Add CORS support on the API

--- Program.cs
+++ Program.cs
@@ -18,9 +18,11 @@ namespace eCommerce
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddScoped<IProductRepository, ProductRepository>();
             builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-           
+            builder.Services.AddCors();
+
             var app = builder.Build();
             app.UseMiddleware<ExceptionMiddleware>();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
             app.MapControllers();
             try
             {
