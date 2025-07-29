d001e93a67a2ffd98e7ce2f3d9527331bf09b00b
umut <alizzaade@users.noreply.github.com>
2025-07-29 17:58:51 +0200

    Display products

---
+++ client/src/app/features/shop/shop.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ShopService } from '../../core/services/shop.service';
+import { Product } from '../../../shared/models/product';
+import { ProductItemComponent } from './product-item/product-item.component';
+
+@Component({
+  selector: 'app-shop',
+  imports: [ProductItemComponent],
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.scss',
+})
+export class ShopComponent implements OnInit {
+  private shopService = inject(ShopService);
+  protected readonly title = 'eCommerce';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.initializeShop();
+  }
+
+  initializeShop() {
+    this.shopService.getBrands();
+    this.shopService.getTypes();
+    this.shopService.getProducts().subscribe({
+      next: (response) => (this.products = response.data),
+      error: (error) => console.log(error),
+    });
+  }
+}
