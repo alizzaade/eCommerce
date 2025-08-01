03f39c39e227494532f02d63990ffe9dc149c5b3
umut <alizzaade@users.noreply.github.com>
2025-08-01 12:47:35 +0200

    Set up navigation links

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIcon, MatButton, MatBadge],
+  imports: [MatIcon, MatButton, MatBadge, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
