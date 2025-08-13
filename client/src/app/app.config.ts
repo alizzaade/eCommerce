621d316ebae7049b3908d6feed1937a655f6cf16
umut <alizzaade@users.noreply.github.com>
2025-08-13 18:20:31 +0200

    Add cart persistence

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,8 @@
 import {
+  APP_INITIALIZER,
   ApplicationConfig,
+  inject,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
@@ -9,6 +12,8 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './interceptors/error-interceptor';
 import { loadingInterceptor } from './interceptors/loading-interceptor';
+import { lastValueFrom } from 'rxjs';
+import { InitService } from './services/init.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,5 +21,18 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+      return lastValueFrom(initService.init()).finally(() => {
+        const splash = document.querySelector('#initial-splash');
+        if (splash) {
+          splash.remove();
+        }
+      });
+    }),
+    // {
+    //   provide: APP_INITIALIZER,
+    //   useFactory
+    // },
   ],
 };
