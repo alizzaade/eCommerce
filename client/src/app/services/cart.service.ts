12726306088e9047ec3da83df1c5c14ef0aeadbd
umut <alizzaade@users.noreply.github.com>
2025-08-13 18:23:41 +0200

    Update navbar with cart item count

--- client/src/app/services/cart.service.ts
+++ client/src/app/services/cart.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Cart, CartItem } from '../../shared/models/cart';
 import { Product } from '../../shared/models/product';
@@ -12,6 +12,9 @@ export class CartService {
   baseURl = environment.apiURL;
   private http = inject(HttpClient);
   cart = signal<Cart | null>(null);
+  itemCount = computed(() => {
+    return this.cart()?.items.reduce((sum, item) => sum + item.quantity, 0);
+  });
 
   getCart(id: string) {
     return this.http.get<Cart>(this.baseURl + 'cart?id=' + id).pipe(
