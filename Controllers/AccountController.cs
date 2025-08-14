a08f5c50ad2bf8652547d522b367f0d7b9fc03e3
umut <alizzaade@users.noreply.github.com>
2025-08-14 19:19:24 +0200

    Add custom register endpoint

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using eCommerce.DTOs;
+using eCommerce.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class AccountController(SignInManager<AppUser> signInManager) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser
+            {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+
+            var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            return Ok();
+        }
+    }
+}
---
+++ DTOs/RegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
